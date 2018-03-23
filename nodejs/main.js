a91c3124d49c151417045f647451736a02dbc23f
Roberts Kenins <robs99@inbox.lv>
2018-03-23 13:34:29 +0200

    Viskautkas ar JSON saistits, bet iet tikai pa pusei

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,6 +1,23 @@
+var express = require('express');
+var app = express();
 var fs = require("fs");
+var obj;
 
-var data = fs.readFileSync('input.txt');
+app.get('/listSongs', function (req, res) {
+   fs.readFile( __dirname + "/" + "songs.json", 'utf8', function (err, data) {
+       console.log( data );
+       obj = JSON.parse(data);
+       res.end( data );
+   });
+})
 
-console.log(data.toString());
-console.log("Program Ended");
+console.log(obj);
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
